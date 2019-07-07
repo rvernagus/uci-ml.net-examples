fb9142532c749749c0ccf159cb6abcc64f197f03
Ray Vernagus <r.vernagus@gmail.com>
2019-07-07 09:30:13 -0400

    Add Annealing dataset example.

---
+++ examples/Annealing/Annealing.fs
@@ -0,0 +1,86 @@
+﻿module Annealing
+open Microsoft.ML.Data
+
+[<CLIMutable>]
+type AnnealingData =
+    {
+        [<LoadColumn(0)>]
+        Family : string
+        [<LoadColumn(1)>]
+        ProductType : string
+        [<LoadColumn(2)>]
+        Steel : string
+        [<LoadColumn(3)>]
+        Carbon : float32
+        [<LoadColumn(4)>]
+        Hardness : float32
+        [<LoadColumn(5)>]
+        TemperRolling : string
+        [<LoadColumn(6)>]
+        Condition : string
+        [<LoadColumn(7)>]
+        Formability : string
+        [<LoadColumn(8)>]
+        Strength : float32
+        [<LoadColumn(9)>]
+        NonAgeing : string
+        [<LoadColumn(10)>]
+        SurfaceFinish : string
+        [<LoadColumn(11)>]
+        SurfaceQuality : string
+        [<LoadColumn(12)>]
+        Enamelability : string
+        [<LoadColumn(13)>]
+        Bc : string
+        [<LoadColumn(14)>]
+        Bf : string
+        [<LoadColumn(15)>]
+        Bt : string
+        [<LoadColumn(16)>]
+        BwMe : string
+        [<LoadColumn(17)>]
+        Bl : string
+        [<LoadColumn(18)>]
+        M : string
+        [<LoadColumn(19)>]
+        Chrom : string
+        [<LoadColumn(20)>]
+        Phos : string
+        [<LoadColumn(21)>]
+        Cbond : string
+        [<LoadColumn(22)>]
+        Marvi : string
+        [<LoadColumn(23)>]
+        Exptl : string
+        [<LoadColumn(24)>]
+        Ferro : string
+        [<LoadColumn(25)>]
+        Corr : string
+        [<LoadColumn(26)>]
+        BlueBrightVarnClean : string
+        [<LoadColumn(27)>]
+        Lustre : string
+        [<LoadColumn(28)>]
+        Jurofm : string
+        [<LoadColumn(29)>]
+        S : string
+        [<LoadColumn(30)>]
+        P : string
+        [<LoadColumn(31)>]
+        Shape : string
+        [<LoadColumn(32)>]
+        Thick : float32
+        [<LoadColumn(33)>]
+        Width : float32
+        [<LoadColumn(34)>]
+        Len : float32
+        [<LoadColumn(35)>]
+        Oil : string
+        [<LoadColumn(36)>]
+        Bore : string
+        [<LoadColumn(37)>]
+        Packing : string
+        [<LoadColumn(38)>]
+        [<ColumnName("Label")>]
+        Classes : string
+    }
---
+++ examples/Annealing/Program.fs
@@ -0,0 +1,90 @@
+﻿open Microsoft.ML
+open Microsoft.ML.Data
+open System.IO
+open System.Net
+open Annealing
+
+
+let append (chain : EstimatorChain<'T>) transform =
+    chain.Append(transform)
+
+let onehot (context : MLContext) (column : string) =
+    context.Transforms.Categorical.OneHotEncoding(column)
+
+let concatenate (context : MLContext) outputColumnName inputColumnNames =
+    context.Transforms.Concatenate(outputColumnName = outputColumnName, inputColumnNames = inputColumnNames)
+
+let mapValueToKey (context : MLContext) sourceColumn destinationColumn =
+    context.Transforms.Conversion.MapValueToKey(inputColumnName = sourceColumn, outputColumnName = destinationColumn)
+
+let minMax (context : MLContext) inputColumn outputColumn =
+    context.Transforms.NormalizeMinMax(outputColumnName = outputColumn, inputColumnName = inputColumn)
+
+let printMetrics (metrics : MulticlassClassificationMetrics) =
+    printfn "Accuracy: %f" metrics.MacroAccuracy
+    printfn "Log Loss: %f" metrics.LogLoss
+    printfn "Confusion Matrix:"
+    printfn "%s" <| metrics.ConfusionMatrix.GetFormattedConfusionTable()
+
+let downcastEstimator (e : IEstimator<'a>) =
+    match e with
+    | :? IEstimator<ITransformer> as p -> p
+    | _ -> failwith "The estimator has to be an instance of IEstimator<ITransformer>."
+
+
+[<EntryPoint>]
+let main argv =
+    if not <| File.Exists("anneal.data") then
+        use client = new WebClient()
+        client.DownloadFile("https://archive.ics.uci.edu/ml/machine-learning-databases/annealing/anneal.data", "anneal.data")
+
+    if not <| File.Exists("anneal.test") then
+        use client = new WebClient()
+        client.DownloadFile("https://archive.ics.uci.edu/ml/machine-learning-databases/annealing/anneal.test", "anneal.test")
+
+    let context = new MLContext()
+
+    let trainDataView =
+        context.Data.LoadFromTextFile<AnnealingData>("anneal.data", hasHeader = false, separatorChar = ',')
+        |> context.Data.ShuffleRows
+
+    let testDataView =
+        context.Data.LoadFromTextFile<AnnealingData>("anneal.test", hasHeader = false, separatorChar = ',')
+        |> context.Data.ShuffleRows
+
+    let featureColumns =
+        [|
+            "Family"; "ProductType"; "Steel"; "Carbon"; "Hardness"; "TemperRolling"; "Condition"; "Formability"; "Strength";
+            "NonAgeing"; "SurfaceFinish"; "SurfaceQuality"; "Enamelability"; "Bc"; "Bf"; "Bt"; "BwMe"; "Bl"; "M"; "Chrom";
+            "Phos"; "Cbond"; "Marvi"; "Exptl"; "Ferro"; "Corr"; "BlueBrightVarnClean"; "Lustre"; "Jurofm"; "S"; "P"; "Shape";
+            "Thick"; "Width"; "Len"; "Oil"; "Bore"; "Packing"
+        |]
+    let categoricalColumns =
+        [|
+            "Family"; "ProductType"; "Steel"; "TemperRolling"; "Condition"; "Formability"; "NonAgeing"; "SurfaceFinish";
+            "SurfaceQuality"; "Enamelability"; "Bc"; "Bf"; "Bt"; "BwMe"; "Bl"; "M"; "Chrom"; "Phos"; "Cbond"; "Marvi";
+            "Exptl"; "Ferro"; "Corr"; "BlueBrightVarnClean"; "Lustre"; "Jurofm"; "S"; "P"; "Shape"; "Oil"; "Bore"; "Packing"
+        |]
+
+    let transformer =
+        categoricalColumns
+        |> Seq.map (onehot context)
+        |> Seq.fold append (EstimatorChain())
+        |> append <| mapValueToKey context "Label" "Label"
+        |> append <| concatenate context "Features" featureColumns
+        |> append <| minMax context "Features" "FeaturesNorm"
+        |> (fun pipeline -> pipeline.Fit(trainDataView))
+
+    let transformedTrainData = transformer.Transform(trainDataView)
+    let transformedTestData = transformer.Transform(testDataView)
+
+    let estimator = context.MulticlassClassification.Trainers.LbfgsMaximumEntropy(featureColumnName = "Features")
+    let finalEstimator = downcastEstimator estimator
+
+    context.MulticlassClassification.CrossValidate(transformedTrainData, finalEstimator, numberOfFolds = 3)
+    |> Seq.maxBy (fun cvResult -> cvResult.Metrics.MacroAccuracy)
+    |> fun cvResult -> cvResult.Model.Transform(transformedTestData)
+    |> context.MulticlassClassification.Evaluate
+    |> printMetrics
+
+    0
