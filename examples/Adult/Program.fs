305c0f43cac157591a1d501b172386d8272ee90e
Ray Vernagus <r.vernagus@gmail.com>
2019-07-18 20:54:34 -0400

    Clean up code and add comments

--- examples/Adult/Program.fs
+++ examples/Adult/Program.fs
@@ -7,20 +7,24 @@ open System.Collections.Generic
 
 
 let printCvResultMetrics (cvResults : TrainCatalogBase.CrossValidationResult<CalibratedBinaryClassificationMetrics> seq) =
-    printfn "------------------\nCross Validation Metrics\n------------------"
+    do
+        printfn "------------------\nCross Validation Metrics\n------------------"
+        cvResults
+        |> Seq.map (fun cvResult -> cvResult.Metrics.Accuracy)
+        |> Seq.average
+        |> printfn "Accuracy: %f"; cvResults
+        |> Seq.map (fun cvResult -> cvResult.Metrics.AreaUnderRocCurve)
+        |> Seq.average
+        |> printfn "Area Under Roc Curve: %f"; cvResults
+        |> Seq.map (fun cvResult -> cvResult.Metrics.F1Score)
+        |> Seq.average
+        |> printfn "F1 Score: %f"
+
     cvResults
-    |> Seq.map (fun cvResult -> cvResult.Metrics.Accuracy)
-    |> Seq.average
-    |> printfn "Accuracy: %f"
-    cvResults
-    |> Seq.map (fun cvResult -> cvResult.Metrics.AreaUnderRocCurve)
-    |> Seq.average
-    |> printfn "Area Under Roc Curve: %f"
-    cvResults
-    |> Seq.map (fun cvResult -> cvResult.Metrics.F1Score)
-    |> Seq.average
-    |> printfn "F1 Score: %f"
-    cvResults
+
+let loadAndShuffle (context : MLContext) filePath =
+    context.Data.LoadFromTextFile<AdultData>(filePath, hasHeader = false, separatorChar = ',')
+    |> context.Data.ShuffleRows
 
 let append (chain : EstimatorChain<'T>) transform =
     chain.Append(transform)
@@ -34,7 +38,7 @@ let concatenate (context : MLContext)  outputColumnName inputColumnNames =
 let mapValue (context : MLContext)  outputColumnName (keyValuePairs : IEnumerable<KeyValuePair<string, bool>>) inputColumnName =
     context.Transforms.Conversion.MapValue(outputColumnName, keyValuePairs, inputColumnName)
 
-let bin (context : MLContext)  inputColumn outputColumn =
+let normalize (context : MLContext)  inputColumn outputColumn =
     context.Transforms.NormalizeBinning(outputColumnName = outputColumn, inputColumnName = inputColumn)
 
 let downcastEstimator (e : IEstimator<'a>) =
@@ -42,12 +46,23 @@ let downcastEstimator (e : IEstimator<'a>) =
     | :? IEstimator<ITransformer> as p -> p
     | _ -> failwith "The estimator has to be an instance of IEstimator<ITransformer>."
 
+let makeEstimator (context : MLContext) featureColumnName =
+    context.BinaryClassification.Trainers.SdcaLogisticRegression(featureColumnName = featureColumnName)
+    |> downcastEstimator
+
+let transform (transformer : ITransformer) dataView =
+    transformer.Transform(dataView)
+
+let crossValidate (context : MLContext) estimator numberOfFolds dataView =
+    context.BinaryClassification.CrossValidate(dataView, estimator, numberOfFolds = numberOfFolds)
+
 let printMetrics (metrics : CalibratedBinaryClassificationMetrics) =
-    printfn "------------------\nTest Metrics\n------------------"
-    printfn "Accuracy: %f" metrics.Accuracy
-    printfn "Log Loss: %f" metrics.LogLoss
-    printfn "Area Under ROC Curve: %f" metrics.AreaUnderRocCurve
-    printfn "F1 Score: %f" metrics.F1Score
+    do
+        printfn "------------------\nTest Metrics\n------------------"
+        printfn "Accuracy: %f" metrics.Accuracy
+        printfn "Log Loss: %f" metrics.LogLoss
+        printfn "Area Under ROC Curve: %f" metrics.AreaUnderRocCurve
+        printfn "F1 Score: %f" metrics.F1Score
 
 
 [<EntryPoint>]
@@ -62,12 +77,8 @@ let main argv =
     
     let context = new MLContext()
     
-    let trainDataView =
-        context.Data.LoadFromTextFile<AdultData>("adult.data", hasHeader = false, separatorChar = ',')
-        |> context.Data.ShuffleRows
-    let testDataView =
-        context.Data.LoadFromTextFile<AdultData>("adult.test", hasHeader = false, separatorChar = ',')
-        |> context.Data.ShuffleRows
+    let trainDataView = loadAndShuffle context "adult.data"
+    let testDataView = loadAndShuffle context "adult.test"
     
     let featureColumns = [| "Age"; "WorkClass"; "Fnlwgt"; "Education"; "EducationNum"; "MaritalStatus"; "Occupation"; "Relationship"; "Race"; "Sex"; "CapitalGain"; "CapitalLoss"; "HoursPerWeek"; "NativeCountry" |]
     let categoricalColumns = [| "WorkClass"; "Education"; "MaritalStatus"; "Occupation"; "Relationship"; "Race"; "Sex"; "NativeCountry" |]
@@ -79,27 +90,26 @@ let main argv =
             KeyValuePair(">50K.", true)
         |]
 
-    let pipeline =
+    let transformer =
         categoricalColumns
-        |> Seq.map (onehot context)
-        |> Seq.fold append (EstimatorChain())
-        |> append <| mapValue context "Label" labelLookup "Label"
-        |> append <| concatenate context "Features" featureColumns
-        |> append <| bin context "Features" "FeaturesNorm"
-
-    let transformer = pipeline.Fit(trainDataView)
-    let transformedTrainData = transformer.Transform(trainDataView)
-    let transformedTestData = transformer.Transform(testDataView)
+        |> Seq.map (onehot context) // Create a one-hot encoder for each categorical column
+        |> Seq.fold append (EstimatorChain()) // Add the encoders to a new EstimatorChain
+        |> append <| mapValue context "Label" labelLookup "Label" // Map labels to either true or false
+        |> append <| concatenate context "Features" featureColumns // Concatenate feature columns into a single new column
+        |> append <| normalize context "Features" "FeaturesNorm" // Normalize features into a new column, FeaturesNorm
+        |> (fun pipeline -> pipeline.Fit(trainDataView)) // Fit our pipeline on the training data
     
-    let estimator = context.BinaryClassification.Trainers.SdcaLogisticRegression(featureColumnName = "FeaturesNorm")
-    let finalEstimator = downcastEstimator estimator
+    let estimator = makeEstimator context "FeaturesNorm"
     
     do
-        context.BinaryClassification.CrossValidate(transformedTrainData, finalEstimator, numberOfFolds = 3)
-        |> printCvResultMetrics
-        |> Seq.maxBy (fun cvResult -> cvResult.Metrics.Accuracy)
-        |> fun cvResult -> cvResult.Model.Transform(transformedTestData)
-        |> context.BinaryClassification.Evaluate
+        trainDataView  // Begin with the training data
+        |> transform transformer // Transform using the transformer built above
+        |> crossValidate context estimator 3 // 3-fold cross-validation
+        |> printCvResultMetrics // Print cross-fold metrics
+        |> Seq.maxBy (fun cvResult -> cvResult.Metrics.Accuracy)  // Select the best model by accuracy
+        |> fun cvResult -> cvResult.Model
+        |> transform <| transform transformer testDataView // Transform the test data and get predictions
+        |> context.BinaryClassification.Evaluate // Get test metrics
         |> printMetrics
 
     0
