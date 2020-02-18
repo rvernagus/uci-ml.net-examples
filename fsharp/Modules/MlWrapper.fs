b2f3697e822fee41ca3d7f14afa8f8ef1fc563c5
Ray Vernagus <r.vernagus@gmail.com>
2020-02-18 14:08:09 -0500

    Add Abalone F# notebook

--- fsharp/Modules/MlWrapper.fs
+++ fsharp/Modules/MlWrapper.fs
@@ -3,85 +3,82 @@ open Microsoft.ML
 open Microsoft.ML.Data
 open System.Collections.Generic
 
-type MlWrapper() =
+module ML =
     let context = MLContext()
 
-    member _.Context
-        with get() = context
-
-    member _.Append (chain : EstimatorChain<'T>) transform =
+    let append (chain : EstimatorChain<'T>) transform =
         chain.Append(transform)
 
-    member _.Shuffle dataView =
-        context.Data.ShuffleRows(dataView)
-
-    member _.Split testFraction dataView =
-        let splitData = context.Data.TrainTestSplit(dataView, testFraction = testFraction)
-        splitData.TrainSet, splitData.TestSet
-
-    member _.Onehot (column : string) =
-        context.Transforms.Categorical.OneHotEncoding(column)
-
-    member _.Concatenate outputColumn inputColumns =
+    let concatenate outputColumn inputColumns =
         context.Transforms.Concatenate(outputColumnName = outputColumn, inputColumnNames = inputColumns)
 
-    member _.MapValue outputColumn (keyValuePairs : IEnumerable<KeyValuePair<string, bool>>) inputColumn =
-        context.Transforms.Conversion.MapValue(outputColumn, keyValuePairs, inputColumn)
+    let crossValidateRegression estimator numberOfFolds dataView =
+        context.Regression.CrossValidate(dataView, estimator, numberOfFolds = numberOfFolds)
 
-    member _.MapValueToKey inputColumn outputColumn =
-        context.Transforms.Conversion.MapValueToKey(outputColumnName = outputColumn, inputColumnName = inputColumn)
+    let crossValidateBinaryClassification estimator numberOfFolds dataView =
+        context.BinaryClassification.CrossValidate(dataView, estimator, numberOfFolds = numberOfFolds)
 
-    member _.MapKeyToValue inputColumn outputColumn =
-        context.Transforms.Conversion.MapKeyToValue(outputColumn, inputColumnName = inputColumn)
+    let crossValidateMulticlassClassification estimator numberOfFolds dataView =
+        context.MulticlassClassification.CrossValidate(dataView, estimator, numberOfFolds = numberOfFolds)
 
-    member _.NormalizeLp inputColumn outputColumn  =
-        context.Transforms.NormalizeLpNorm(outputColumnName = outputColumn, inputColumnName = inputColumn)
-
-    member _.NormalizeMinMax inputColumn outputColumn  =
-        context.Transforms.NormalizeMinMax(outputColumnName = outputColumn, inputColumnName = inputColumn)
-
-    member _.Fit<'T when 'T :> ITransformer and 'T : not struct > dataView (pipeline : EstimatorChain<'T>) =
-        pipeline.Fit(dataView)
-
-    member _.DowncastEstimator (e : IEstimator<'a>) =
+    let downcastEstimator (e : IEstimator<'a>) =
         match e with
         | :? IEstimator<ITransformer> as p -> p
         | _ -> failwith "The estimator has to be an instance of IEstimator<ITransformer>."
 
-    member _.Transform (transformer : ITransformer) dataView =
+    let fit<'T when 'T :> ITransformer and 'T : not struct > dataView (pipeline : EstimatorChain<'T>) =
+        pipeline.Fit(dataView)
+
+    let mapValue outputColumn (keyValuePairs : IEnumerable<KeyValuePair<string, bool>>) inputColumn =
+        context.Transforms.Conversion.MapValue(outputColumn, keyValuePairs, inputColumn)
+
+    let mapValueToKey inputColumn outputColumn =
+        context.Transforms.Conversion.MapValueToKey(outputColumnName = outputColumn, inputColumnName = inputColumn)
+
+    let mapKeyToValue inputColumn outputColumn =
+        context.Transforms.Conversion.MapKeyToValue(outputColumn, inputColumnName = inputColumn)
+
+    let normalizeLp inputColumn outputColumn  =
+        context.Transforms.NormalizeLpNorm(outputColumnName = outputColumn, inputColumnName = inputColumn)
+
+    let normalizeMinMax inputColumn outputColumn  =
+        context.Transforms.NormalizeMinMax(outputColumnName = outputColumn, inputColumnName = inputColumn)
+
+    let onehot (column : string) =
+        context.Transforms.Categorical.OneHotEncoding(column)
+
+    let shuffle dataView =
+        context.Data.ShuffleRows(dataView)
+
+    let split testFraction dataView =
+        let splitData = context.Data.TrainTestSplit(dataView, testFraction = testFraction)
+        splitData.TrainSet, splitData.TestSet
+
+    let transform (transformer : ITransformer) dataView =
         transformer.Transform(dataView)
 
-    member _.CrossValidateRegression estimator numberOfFolds dataView =
-        context.Regression.CrossValidate(dataView, estimator, numberOfFolds = numberOfFolds)
-
-    member _.CrossValidateBinaryClassification estimator numberOfFolds dataView =
-        context.BinaryClassification.CrossValidate(dataView, estimator, numberOfFolds = numberOfFolds)
-
-    member _.CrossValidateMulticlassClassification estimator numberOfFolds dataView =
-        context.MulticlassClassification.CrossValidate(dataView, estimator, numberOfFolds = numberOfFolds)
-
-    member _.PrintRegressionMetrics (metrics : RegressionMetrics) =
+    let printRegressionMetrics (metrics : RegressionMetrics) =
         printfn "------------------\nTest Metrics\n------------------"
         printfn "Mean Absolute Error: %f" metrics.MeanAbsoluteError
         printfn "Mean Squared Error: %f" metrics.MeanSquaredError
         printfn "Root Mean Squared Error: %f" metrics.RootMeanSquaredError
         printfn "R-squared: %f" metrics.RSquared
 
-    member _.PrintBinaryClassificationMetrics (metrics : CalibratedBinaryClassificationMetrics) =
+    let printBinaryClassificationMetrics (metrics : CalibratedBinaryClassificationMetrics) =
         printfn "------------------\nTest Metrics\n------------------"
         printfn "Accuracy: %f" metrics.Accuracy
         printfn "Log Loss: %f" metrics.LogLoss
         printfn "Area Under ROC Curve: %f" metrics.AreaUnderRocCurve
         printfn "F1 Score: %f" metrics.F1Score
 
-    member _.PrintMulticlassClassificationMetrics (metrics : MulticlassClassificationMetrics) =
+    let printMulticlassClassificationMetrics (metrics : MulticlassClassificationMetrics) =
         printfn "------------------\nTest Metrics\n------------------"
         printfn "Accuracy: %f" metrics.MacroAccuracy
         printfn "Log Loss: %f" metrics.LogLoss
         printfn "Confusion Matrix:"
         printfn "%s" <| metrics.ConfusionMatrix.GetFormattedConfusionTable()
 
-    member _.PrintRegressionCvMetrics (cvResults : TrainCatalogBase.CrossValidationResult<RegressionMetrics> seq) =
+    let printRegressionCvMetrics (cvResults : TrainCatalogBase.CrossValidationResult<RegressionMetrics> seq) =
         printfn "------------------\nCross Validation Metrics\n------------------"
         cvResults
         |> Seq.map (fun cvResult -> cvResult.Metrics.MeanAbsoluteError)
@@ -99,7 +96,7 @@ type MlWrapper() =
 
         cvResults
 
-    member _.PrintBinaryClassificationCvMetrics(cvResults : TrainCatalogBase.CrossValidationResult<CalibratedBinaryClassificationMetrics> seq) =
+    let printBinaryClassificationCvMetrics(cvResults : TrainCatalogBase.CrossValidationResult<CalibratedBinaryClassificationMetrics> seq) =
         printfn "------------------\nCross Validation Metrics\n------------------"
         cvResults
         |> Seq.map (fun cvResult -> cvResult.Metrics.Accuracy)
@@ -114,7 +111,7 @@ type MlWrapper() =
 
         cvResults
 
-    member _.PrintMulticlassClassificationCvMetrics(cvResults: TrainCatalogBase.CrossValidationResult<MulticlassClassificationMetrics> seq) =
+    let printMulticlassClassificationCvMetrics(cvResults: TrainCatalogBase.CrossValidationResult<MulticlassClassificationMetrics> seq) =
         printfn "------------------\nCross Validation Metrics\n------------------"
         cvResults
         |> Seq.map (fun cvResult -> cvResult.Metrics.MacroAccuracy)
